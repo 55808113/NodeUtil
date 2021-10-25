3b56677e6fcb738b9949afca49fb8ab871e2f2ee
sunxdd <sunxdd@163.com>
2021-10-25 15:53:06 +0800

    OK

--- token.js
+++ token.js
@@ -6,6 +6,7 @@ const url = require('url')
 const $log4js = require('./log4js')
 const $convert = require('./convert')
 const $crypto = require('./crypto')
+const $util = require('./util')
 const token = {
     /**
      * 得到Token字符串
@@ -13,27 +14,7 @@ const token = {
      * @returns {string} 如果是null，表示没有取得
      */
     getTokenString: function (ctx){
-        return ctx.request.body.token || ctx.request.query.token || ctx.request.headers['Authorization'] || ctx.request.headers['token'] || ctx.session.token || ctx.cookies.get('token');
-    },
-    /**
-     * 创建Token
-     * @param {object} obj 数据对象
-     * @param {int} timeout 过期时间：默认有效期为一天
-     * @returns {string}
-     */
-    createToken: function (obj, timeout = 24 * 60 * 60) {
-        //console.log(parseInt(timeout)||0);
-        let obj2 = {
-            data: obj,//payload
-            created: parseInt(Date.now() / 1000),//token生成的时间的，单位秒
-            exp: parseInt(timeout) || 24 * 60 * 60//token有效期为一天
-        };
-
-        //payload信息
-        let base64Str = Buffer.from(JSON.stringify(obj2), "utf8").toString("base64");
-        //添加签名，防篡改
-        let signature = $crypto.SHA.HmacSHA256(base64Str)
-        return base64Str + "." + signature;
+        return ctx.session.token || ctx.request.body.token || ctx.request.query.token || ctx.request.headers['Authorization'] || ctx.request.headers['token'] || ctx.cookies.get('token');
     },
     /**
      * 解密Token
@@ -56,7 +37,7 @@ const token = {
      * }
      *
      */
-    decodeToken: function (token) {
+    _decode: function (token) {
         let decArr = $convert.strToArray(token, ".")
         if (decArr.length < 2) {
             //token不合法
@@ -83,50 +64,99 @@ const token = {
             checkSignature: checkSignature
         }
     },
+    /**
+     * 创建Token
+     * @param {object} obj 数据对象
+     * @param {int} timeout 过期时间：默认有效期为一天
+     * @returns {string}
+     */
+    create: function (obj, timeout = 24 * 60 * 60) {
+        //console.log(parseInt(timeout)||0);
+        let obj2 = {
+            data: obj,//payload
+            created: parseInt(Date.now() / 1000),//token生成的时间的，单位秒
+            exp: parseInt(timeout) || 24 * 60 * 60//token有效期为一天
+        };
 
+        //payload信息
+        let base64Str = Buffer.from(JSON.stringify(obj2), "utf8").toString("base64");
+        //添加签名，防篡改
+        let signature = $crypto.SHA.HmacSHA256(base64Str)
+        return base64Str + "." + signature;
+    },
     /**
      * 得到Token的返回的对象
      * @param {string} token
      * @returns {object} 如果是null，表示没有取得
      */
-    getTokenData: function (ctx){
-        let token = this.getTokenString(ctx)
+    getToken: function (token){
         if (!token) return null;
-        let resDecode = this.decodeToken(token);
+        let resDecode = this._decode(token);
         if (!resDecode) {
             return null;
         }
         //是否过期
         let expState = (parseInt(Date.now() / 1000) - parseInt(resDecode.payload.created)) > parseInt(resDecode.payload.exp) ? false : true;
         if (resDecode.signature === resDecode.checkSignature && expState) {
-            let data = resDecode.payload.data
-            ctx.session.pkid = data.pkid;
-            ctx.session.cd_info_ssgx = data.cd_info_ssgx;
-            ctx.session.usernm = data.usernm;
-            /*//如果cookies没有设置就设置给html
-            if (ctx.session.token != token) {
-                ctx.session.token = token;
-                ctx.session.pkid = resDecode.payload.data.pkid;
-                ctx.session.cd_info_ssgx = resDecode.payload.data.cd_info_ssgx;
-                ctx.session.usernm = resDecode.payload.data.usernm;
-                ctx.session.rights = $convert.strToArray(resDecode.payload.data.rights, ",");
-            }
-            ctx.cookies.set('token', token, {signed: true, maxAge: 7200000})*/
-            return data;
+            return resDecode;
         }
         return null;
     },
     /**
-     * 检查Token
-     * @param ctx
-     * @returns {boolean} 是否成功
+     * 得到Token的返回的对象
+     * @param {string} token
+     * @returns {object} 如果是null，表示没有取得
      */
-    checkToken: function (ctx) {
-        let result = this.getTokenData(ctx)
-        if (result) {
-            return true;
+    checkToken: function (ctx){
+        if (ctx.session.token){
+            return true
         }
-        return false;
+        let token = this.getTokenString(ctx)
+        let resDecode = this.getToken(token)
+        if (!resDecode) return false
+        return true
+    },
+    /**
+     * 设置session的对象
+     * @param {string} token
+     * @returns {object} 如果是null，表示没有取得
+     */
+    setSessionToken: function (ctx){
+        let token = this.getTokenString(ctx)
+        if (ctx.session.token != token) {
+            let resDecode = this.getToken(token)
+            if (!resDecode) return
+            let data = resDecode.payload.data
+            ctx.session.token = token;
+            ctx.session.pkid = data.pkid;
+            //ctx.session.cd_info_ssgx = data.cd_info_ssgx;
+            ctx.session.usernm = data.usernm;
+        }
+    },
+    /**
+     * 建立cookie
+     * @param ctx
+     */
+    setCookieToken: function (ctx){
+        let token = this.getTokenString(ctx)
+        ctx.cookies.set('token', token, {signed: true, maxAge: 7200000})
+    },
+    /**
+     * 得到用户pkid
+     * @returns {Promise<string>}
+     */
+    getUserPkid: function (ctx) {
+        let pkid = ""
+        if (!$util.isEmpty(ctx.session.pkid)){
+            pkid = ctx.session.pkid
+        }else{
+            let token = this.getTokenString(ctx)
+            let resDecode = this.getToken(token)
+            if (!resDecode) return pkid
+            let data = resDecode.payload.data
+            pkid = data.pkid
+        }
+        return pkid
     }
 }
 module.exports = token;
